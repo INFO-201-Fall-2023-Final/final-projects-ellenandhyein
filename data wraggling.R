24e7998a91c483a9a9f462276c0834605512a1d3
hyeinhwang <149348736+hyeinhwang@users.noreply.github.com>
2023-11-27 17:55:38 -0800

    Update data wraggling.R

--- data wraggling.R
+++ data wraggling.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 eighten_df <- read.csv("2018.csv")
 twenty_df <- read.csv("WHR20_DataForFigure2.1 2.csv")
-df_suicide <- read.csv("Suicide Rate.csv")
+suicide_df <- read.csv("Suicide Rate.csv")
 
 colnames(eighten_df)[colnames(eighten_df) == "Score"] <- "Score.18"
 colnames(eighten_df)[colnames(eighten_df) == "GDP.per.capita"] <- "GDP.per.capita.18"
@@ -14,8 +14,8 @@ colnames(eighten_df)[colnames(eighten_df) == "Freedom.to.make.life.choices"] <- 
 colnames(eighten_df)[colnames(eighten_df) == "Generosity"] <- "Generosity.18"
 colnames(eighten_df)[colnames(eighten_df) == "Perceptions.of.corruption"] <- "Perceptions.of.corruption.18"
 
-colnames(df_suicide)[colnames(df_suicide) == "GDP.per.capita"] <- "GDP.per.capita.suicide"
+colnames(suicide_df)[colnames(suicide_df) == "GDP.per.capita"] <- "GDP.per.capita.suicide"
 
 df <- merge(x= eighten_df, y=twenty_df, by.x= c("Country.or.region"), by.y=c("Country.name"))
 
-df <- merge(x= df, y=df_suicide, by.x= c("Country.or.region"), by.y= c("Country")) 
+df <- merge(x= df, y=suicide_df, by.x= c("Country.or.region"), by.y= c("Country")) 
